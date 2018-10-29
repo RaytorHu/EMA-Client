ab578f3c1dc834e5f9b57181f44bfcaa67ffcf76
bran <brantonli.ca@gmail.com>
2018-10-29 00:11:52 -0700

    implement basic fetch api for requesting data from back end

--- src/container/movie.js
+++ src/container/movie.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 class Movie extends Component {
-  render() {
-    return <div>Movie page</div>;
-  }
+    constructor(props){
+        super(props);
+        this.state={
+
+        };
+    }
+
+    componentWillMount(){
+        const url = 'https://clapperboard-api.azurewebsites.net/v1/movies';
+        fetch(url)
+            .then((response=>response.json()))
+            .then((responseJson)=>{
+
+            })
+    }
+
+    render() {
+        return <div>Movie page</div>;
+    }
 }
 
 export default Movie;
