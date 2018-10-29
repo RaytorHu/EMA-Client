cd31ba2e09061557afea8c2ec8c318fa5755a380
sxa16@sfu.ca <sxa16@sfu.ca>
2018-10-28 23:04:45 -0700

    Adding demoRestaurant json
    change routing of dinning page

--- src/container/dining.js
+++ src/container/dining.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
+import myData from "./data/demoRestaurant.json";
 
 class Dining extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurants: Array(9)
+    };
+  }
+
   render() {
     return (
       <div>
         <h3>Dining page</h3>
+        <p />
       </div>
     );
+  }
+}
+
+class demoRestaurantList extends Component {
+  constructor(props, i) {
+    super(props);
+    this.state = myData.business[i];
   }
 }
 
--- src/layout/routing.js
+++ src/layout/routing.js
@@ -13,6 +13,7 @@ import Expense from "../container/expense";
 import Settings from "../container/settings";
 
 import { Layout, Menu } from "antd";
+import { Link } from "react-router-dom";
 
 const { Content, Sider, Footer } = Layout;
 const MenuItemGroup = Menu.ItemGroup;
@@ -44,10 +45,17 @@ export const BaseLayout = ({ component: Component, ...rest }) => {
                   defaultSelectedKeys={["1"]}
                   style={{ height: "100%", borderRight: 0 }}
                 >
-                  {matchProps.match.path === "/dining" && (
+                  {matchProps.match.path.startsWith("/dining") && (
                     <MenuItemGroup title="Dining">
-                      <Menu.Item key="1">Find Restaurants</Menu.Item>
-                      <Menu.Item key="2">Heat Map</Menu.Item>
+                      <Menu.Item key="1">
+                        <Link to="/dining/find_restaurant">
+                          Find Restaurants
+                        </Link>
+                      </Menu.Item>
+                      <Menu.Item key="2">
+                        <Link to="/dining/restaurant_heatmap">Heat Map</Link>
+                        Heat Map
+                      </Menu.Item>
                     </MenuItemGroup>
                   )}
                   {matchProps.match.path === "/movie" && (
@@ -126,16 +134,21 @@ export class ComponentRoutes extends Component {
   render() {
     return (
       <Switch>
-        <PrivateRoute exact path="/dining" component={Dining} />
+        <PrivateRoute exact path="/dining/find_restaurant" component={Dining} />
+        <PrivateRoute
+          exact
+          path="/dining/restaurant_heatmap"
+          component={Dining}
+        />
 
         <PrivateRoute exact path="/movie" component={Movie} />
 
         <PrivateRoute exact path="/expense" component={Expense} />
 
         <PrivateRoute exact path="/settings" component={Settings} />
-        <Redirect from="/" to="/dining" />
+        <Redirect from="/" to="/dining/find_restaurant" />
 
-        <Redirect from="/dinings" to="/dining" />
+        <Redirect from="/dinings" to="/dining/find_restaurant" />
         <Redirect from="/movies" to="/movie" />
         <Redirect from="/expenses" to="/expense" />
         <Redirect from="/setting" to="/settings" />
