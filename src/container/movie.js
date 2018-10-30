cb3aa030edc168ab000a4413bc98294d4d8ca9d0
bran <brantonli.ca@gmail.com>
2018-10-29 22:01:09 -0700

    import movie data and extract it to use
    use local data instead of requesting from cineplex API as it is blocked. extract saved DB data to movie class and showing it simply on the view.

--- src/container/movie.js
+++ src/container/movie.js
@@ -1,42 +1,49 @@
 import React, { Component } from "react";
+import MovieList from "./data/moviesDB.json";
+import { List, Avatar, Icon, Rate } from "antd";
+
 class Movie extends Component {
     constructor(props){
         super(props);
         this.state={
-            data:[]
+            data: []
         };
     }
 
-    componentWillMount(){
-        //https://clapperboard-api.azurewebsites.net/v1/movies
-        const url = 'https://facebook.github.io/react-native/movies.json';
-        fetch(url)
-            .then((response)=>response.json())
-            .then((responseJson)=>{
-                console.log(responseJson)
-                this.setState({
-                    data: responseJson.movies,
-                })
-                //console.log('in response')
-            })
-            .catch(function(error){
-                //console.log('failure')
-                console.log(JSON.stringify(error));
-            })
-    }
+    extractList(){
+        const tmp = [];
+
+        for(let i=0; i < MovieList.movies.length; i++){
+            tmp.push({
+                MovieID: MovieList.movies[i].id,
+                Moviename: MovieList.movies[i].name,
+                release: MovieList.movies[i].releaseDate,
+                genres: MovieList.movies[i].genres,
+                synopsis: MovieList.movies[i].synopsis,
+                pic: MovieList.movies[i].posterLargeURL,
+                trailer: MovieList.movies[i].trailerURL,
+                Movie_len: MovieList.movies[i].runtime,
+                rate: MovieList.movies[i].rating,
+                weblink: MovieList.movies[i].webURL
+            });
+        }
+
+        return tmp;
+    };
+
+    componentDidMount(){
+        var moviedata =  this.extractList();
+        this.setState({
+            data: moviedata
+        });
+        console.log(this.state.data);
+    };
 
     render() {
+        //console.log(MovieList.movies.length);
         return (
-            <div>
-                {
-                    this.state.data.map((record, key)=>
-                        <div>
-                            <span>{record.title}</span>
-                            <span>{record.releaseYear}</span>
-                        </div>
-                    )
-                }
-            </div>
+            <List dataSource={this.state.data} renderItem={}
+            />
         )
     }
 }
