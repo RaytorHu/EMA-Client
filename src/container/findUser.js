792025b00a631ea320c9a42eed5974ed55ce3af6
jianing <jianings0617@gmail.com>
2018-11-18 15:36:10 -0800

    Added follow component on the client side

--- src/container/findUser.js
+++ src/container/findUser.js
@@ -1,61 +1,70 @@
-import React, { Component } from "react";
-import { Input, Table, Button, Avatar, Tooltip } from 'antd';
-import storage from '../utils/Storage';
-import config from '../config.js';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { Input, Table, Button, Avatar, Tooltip } from 'antd'
+import storage from '../utils/Storage'
+import config from '../config.js'
+import axios from 'axios'
+import Follow from '../component/follow'
 
-const Search = Input.Search;
-const columns = [{
-    render: (text) => <Avatar src={text.avatarUrl} />
-}, {
+const Search = Input.Search
+const columns = [
+  {
+    render: text => <Avatar src={text.avatarUrl} />
+  },
+  {
     title: 'Username',
     dataIndex: 'username',
-    render: text => <a href="javascript:;">{text}</a>,
-},
-{
+    render: text => <a href='javascript:;'>{text}</a>
+  },
+  {
     title: 'User id',
-    dataIndex: "id",
+    dataIndex: 'id',
     render: text => <p>#{text}</p>
-
-}, {
-    render: () => <div><Tooltip placement="right" title="follow">
-        <Button icon="star" />
-    </Tooltip></div>
-}];
+  },
+  {
+    render: text => (
+      <div>
+        <Follow user={text} />
+        {/* <Tooltip placement='right' title='follow'>
+          <Follow user={text} />
+        </Tooltip> */}
+      </div>
+    )
+  }
+]
 
 class FindUser extends Component {
-    state = {
-        users: []
-    }
-    getUserList = async value => {
-        axios({
-            method: 'get',
-            url: config.base_url + 'api/v1/user/' + value,
-            headers: {
-                'Authorization': 'Bearer ' + storage.getAuthToken()
-            }
+  state = {
+    users: []
+  }
+  getUserList = async value => {
+    axios({
+      method: 'get',
+      url: config.base_url + 'api/v1/user/search/' + value,
+      headers: {
+        Authorization: 'Bearer ' + storage.getAuthToken()
+      }
+    }).then(response => {
+      this.setState({
+        users: response.data.data,
+        loading: false
+      })
+    })
+  }
 
-        })
-            .then((response) => {
-                this.setState({
-                    users: response.data.data,
-                    loading: false
-                });
-            })
-    }
-
-    render() {
-        return <div>
-            <h1>Find User</h1>
-            <Search
-                placeholder="name/email/id"
-                onSearch={value => this.getUserList(value)}
-                style={{ width: 200 }}
-            />
-            <br /><br />
-            <Table columns={columns} dataSource={this.state.users} />
-        </div>;
-    }
+  render () {
+    return (
+      <div>
+        <h1>Find User</h1>
+        <Search
+          placeholder='name/email/id'
+          onSearch={value => this.getUserList(value)}
+          style={{ width: 200 }}
+        />
+        <br /><br />
+        <Table columns={columns} dataSource={this.state.users} />
+      </div>
+    )
+  }
 }
 
-export default FindUser;
+export default FindUser
